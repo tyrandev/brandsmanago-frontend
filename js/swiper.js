cca02f9242ec82d7e4fd3e3323e4c0bb612c6666
Tyranowski <andretyranowski@gmail.com>
2025-04-03 20:47:43 +0200

    implement swiper in featured-products

--- js/swiper.js
+++ js/swiper.js
@@ -1,25 +1,57 @@
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".featured-products", {
   loop: true,
-  slidesPerView: 4,
-  spaceBetween: 18,
+  spaceBetween: 30,
   scrollbar: {
     el: ".swiper-scrollbar",
-    hide: false,
+    draggable: true,
+    dragSize: "auto",
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  watchOverflow: true,
   breakpoints: {
     1024: {
       slidesPerView: 4,
     },
-    768: {
+    800: {
       slidesPerView: 3,
     },
-    480: {
+    450: {
       slidesPerView: 1,
+      spaceBetween: 0,
+    },
+  },
+  on: {
+    init: function () {
+      updateNavigationButtons(this);
+    },
+    slideChange: function () {
+      updateNavigationButtons(this);
     },
   },
 });
+
+function updateNavigationButtons(swiper) {
+  const prevButton = document.querySelector(".swiper-button-prev");
+  const nextButton = document.querySelector(".swiper-button-next");
+
+  const firstSlideIndex = 0;
+  const lastSlideIndex = swiper.slides.length - swiper.loopedSlides;
+
+  console.log(
+    `Real Index: ${swiper.realIndex}, Last Slide Index: ${lastSlideIndex}`
+  );
+
+  if (swiper.realIndex === firstSlideIndex) {
+    prevButton.style.display = "none";
+  } else {
+    prevButton.style.display = "flex";
+  }
+
+  if (swiper.realIndex === lastSlideIndex) {
+    nextButton.style.display = "none";
+  } else {
+    nextButton.style.display = "flex";
+  }
+}
